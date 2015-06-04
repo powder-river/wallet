90088c45f147dac2d2b043de4e2e2fe8c6bbbcbe
Danai Adkisson <dadkisso@live.com>
2015-06-03 21:26:18 -0400

    can get biggest expense

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,10 +5,37 @@ class Transaction < ActiveRecord::Base
     self.all.reduce(0) {|sum, t| sum + t.negotiation}
   end
 
-
+  #this works, but doesn't go month specific
   def self.spent_this_month
     var = self.all.select {|n| n.negotiation < 0}
     var.reduce(0) {|sum, t| sum + t.negotiation}
   end
 
+
+  #this one I hate and can't get to work
+
+  # def self.spent_this_month
+  #   var = self.all.select do |d|
+  #     if d.created_at.month == Time.now.month
+  #       if d.negotiation < 0
+  #         d.reduce(0) {|sum, t| sum + t.negotiation}
+  #       end
+  #     end
+  #   end
+  #   var.reduce(0) {|sum, t| sum + t.negotiation}
+
+
+
+
+
+  def get_date_to_work
+
+  end
+
+  def self.biggest_expense
+    array = self.all.map {|n| n.negotiation}
+    array = array.sort
+    array = array[-1]
+    array.to_f
+  end
 end
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -25,4 +25,18 @@ class TransactionTest < ActiveSupport::TestCase
     assert_equal -200, petsmart.negotiation
     assert_equal -300, Transaction.spent_this_month
   end
+
+  test "get date to work please :-)" do
+    petsmart = Transaction.create(name: "PetSmart", negotiation: -200)
+    starbucks = Transaction.create(name: "Starbucks", negotiation: -100)
+    paycheck = Transaction.create(name: "Job", negotiation: 50)
+    assert_equal Time.now.month, petsmart.created_at.month
+  end
+
+  test "get biggest expense" do
+    petsmart = Transaction.create(name: "PetSmart", negotiation: -200)
+    starbucks = Transaction.create(name: "Starbucks", negotiation: -100)
+    paycheck = Transaction.create(name: "Job", negotiation: 50)
+    assert_equal "PetSmart", Transaction.biggest_expense
+  end
 end
